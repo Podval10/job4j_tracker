300fd0890f2efcd9273e0f51f2c1a8fe17e4bc03
andre <kitksiomi@gmail.com>
2026-02-16 15:06:15 +0300

    2. Коллекции, ArrayList, List, Обобщения

---
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+
+public class UsageArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Andrey");
+        names.add("Nikolaevich");
+        names.add("Vasya");
+        for (Object value : names) {
+            System.out.println(value);
+        }
+    }
+}
