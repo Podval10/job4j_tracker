733cfa9886ec8f86f184064b63b20fcb109fa2c0
andre <kitksiomi@gmail.com>
2026-02-19 11:53:34 +0300

    1. Уникальные задачи

---
+++ src/main/java/ru/job4j/collection/FullSearch.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection;
+
+import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+
+public class FullSearch {
+
+    public Set<String> extractNumber(List<Task> tasks) {
+        HashSet<String> result = new HashSet<>();
+        for (Task numbrer : tasks){
+              result.add(numbrer.getNumber());
+
+            }
+
+                 return result;
+    }
+}
