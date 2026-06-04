dc2b4ebfc4d5eea1834695e5556ad25368934053
andre <kitksiomi@gmail.com>
2026-06-05 00:33:27 +0300

    2.2. Функциональный интерфейс

--- src/main/java/ru/job4j/function/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -10,31 +10,32 @@ public class FunctionalInterfaces {
     public static void main(String[] args) {
         Map<Integer, String> map = new HashMap<>();
         BiConsumer<Integer, String> biConsumer = (number,name) -> map.put(number,name);
-        Integer number;
-        String name;
-
-        biConsumer.accept(number,name);
+        biConsumer.accept(1,"one");
+        biConsumer.accept(2,"two");
+        biConsumer.accept(3,"three");
+        biConsumer.accept(4,"four");
+        biConsumer.accept(5,"five");
+        biConsumer.accept(6,"six");
+        biConsumer.accept(7,"seven");
         /*
             Замените вставку элементов в карту на использование BiConsumer, он уже объявлен, требуется его реализовать.
          */
-        map.put(1, "one");
-        map.put(2, "two");
-        map.put(3, "three");
-        map.put(4, "four");
-        map.put(5, "five");
-        map.put(6, "six");
-        map.put(7, "seven");
+//        map.put(1, "one");
+//        map.put(2, "two");
+//        map.put(3, "three");
+//        map.put(4, "four");
+//        map.put(5, "five");
+//        map.put(6, "six");
+//        map.put(7, "seven");
 
-        BiPredicate<Integer, String> biPredicate = (key,string) -> key %2 ==0 || string.length() == 4 ;
-
-
+        BiPredicate<Integer, String> biPredicate = (key,string) -> key % 2 ==0 || string.length() == 4 ;
 
         for (Integer key : map.keySet()) {
-
+//               biPredicate.test(key  ,map.get(key));
             /*
                 Замените проверку в if() на использование BiPredicate, он объявлен выше, требуется его реализовать.
              */
-            if (key % 2 == 0 || map.get(key).length() == 4) {
+            if (biPredicate.test(key  ,map.get(key))) {
                 System.out.println("key: " + key + " value: " + map.get(key));
             }
         }
@@ -42,17 +43,21 @@ public class FunctionalInterfaces {
             Заменить создание ArrayList из значений Map на Supplier, объявлен ниже, требуется его реализовать.
          */
         Supplier<List<String>> supplier = () -> new ArrayList<>(map.values());
-        List<String> strings = new ArrayList<>(map.values());
+//        List<String> strings = new ArrayList<>(map.values());
 
-        Consumer<String> consumer = (string) -> System.out.println(string);
-        Function<String, String> function = ...;
-        for (String string : strings) {
+        Consumer<String> consumer = string -> System.out.println(string);
+        Function<String, String> function = string -> string.toUpperCase();
+        for (String string : supplier.get()) {
+            consumer.accept(string);
+
+
             /*
                 Заменить вывод строк на применение Consumer
                 Заменить преобразование строк к строкам в верхнем регистре с помощью Function
                 Необходимое объявлено выше, требуется их реализовать.
             */
-            System.out.println(string.toUpperCase());
+//            System.out.println(string.toUpperCase());
+            System.out.println(function.apply(string));
         }
     }
 }
